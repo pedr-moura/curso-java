baaa4eb01f07b6de9ac545a72b55d92f5a5d4e9f
Pedro Moura <pedrosousavn@gmail.com>
2025-03-01 13:20:35 -0300

    adicao de construtores com all args

--- src/NivelIntermediario/Construtores/Hokages.java
+++ src/NivelIntermediario/Construtores/Hokages.java
@@ -8,4 +8,21 @@ public class Hokages {
     public Hokages(){
         //contrutor vazio, sem argumentos
     }
+
+    //contrutor com argumento
+    public Hokages(String nome) {
+        this.nome = nome;
+        //this referencia o atributo do inicio do codigo
+    }
+
+    public Hokages(int idade){
+        this.idade = idade;
+    }
+
+    //all args contructor, todos os argumentos *parece uma func, e é
+    public Hokages (String nome, int idade, boolean vivoOuNao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuNao = vivoOuNao;
+    }
 }
--- src/NivelIntermediario/Construtores/Main.java
+++ src/NivelIntermediario/Construtores/Main.java
@@ -7,5 +7,15 @@ public class Main {
 
     public static void main(String[] args) {
         Hokages Hashirama = new Hokages();
+        Hashirama.idade = 45;
+
+        Hokages Tobirama = new Hokages("Tobirama Senju");
+        System.out.println(Tobirama.nome);
+
+        Hokages Hiruzen = new Hokages(59);
+        System.out.println(Hiruzen.idade);
+
+        Hokages Minato = new Hokages("Minato Namikaze", 32, false);
+        System.out.println(Minato.nome);
     }
 }
