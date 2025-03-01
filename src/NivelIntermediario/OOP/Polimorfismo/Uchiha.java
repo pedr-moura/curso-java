245d5d083b907f988d224e750aa0ae53f30e546d
Pedro Moura <pedrosousavn@gmail.com>
2025-03-01 14:26:09 -0300

    adicionando contrutor da subclasse com super

--- src/NivelIntermediario/OOP/Polimorfismo/Uchiha.java
+++ src/NivelIntermediario/OOP/Polimorfismo/Uchiha.java
@@ -2,6 +2,11 @@ package NivelIntermediario.OOP.Polimorfismo;
 
 public class Uchiha extends Ninja{
 
+    //palavra reservada "super", para referenciar a classe Ninja
+    public Uchiha(String nome, int aldeia, int idade) {
+        super(nome, aldeia, idade);
+    }
+
     public void estrategiaDeBatalhaNinja(){
         System.out.println("Essa é minha estrategia de batalha.");
     }
