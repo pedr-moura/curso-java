188c1467eb3ea6ba2f47e9f281e05f984512fe13
Pedro Moura <pedrosousavn@gmail.com>
2025-03-01 13:10:55 -0300

    introducao a construtores

---
+++ src/NivelIntermediario/Construtores/Hokages.java
@@ -0,0 +1,11 @@
+package NivelIntermediario.Construtores;
+
+public class Hokages {
+    String nome;
+    int idade;
+    boolean vivoOuNao;
+
+    public Hokages(){
+        //contrutor vazio, sem argumentos
+    }
+}
---
+++ src/NivelIntermediario/Construtores/Main.java
@@ -0,0 +1,11 @@
+package NivelIntermediario.Construtores;
+
+public class Main {
+
+    //NoArgs Constructor -> sem argumentos
+    //AllArgs Constructor -> todos os argumentos
+
+    public static void main(String[] args) {
+        Hokages Hashirama = new Hokages();
+    }
+}
